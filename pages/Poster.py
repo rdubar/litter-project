8b16a2a5744e27bcdbe54cea77cdec8dee977439
Roger Dubar <rdubar@gmail.com>
2024-04-15 21:32:57 +0100

    Debugging Poster

--- pages/Poster.py
+++ pages/Poster.py
@@ -1,6 +1,22 @@
 import streamlit as st
 from settings import poster_path
 
+import os
+
+# Check if the file exists
+file_path = '/mount/src/litter-project/images/Partick_litter_Plan.jpg'
+if os.path.exists(file_path):
+    st.write('File exists')
+else:
+    st.write('File does not exist')
+    # Optionally list files in the directory
+    directory_path = '/mount/src/litter-project/images/'
+    if os.path.exists(directory_path):
+        files = os.listdir(directory_path)
+        st.write('Files in directory:', files)
+    else:
+        st.write('Directory does not exist')
+
 st.title('Poster 🌿')
 
 st.write(f'{poster_path}.jpg')
