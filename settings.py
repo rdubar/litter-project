5ffe240ce67f90e053ca8688013b1373b0346a41
Roger Dubar <rdubar@gmail.com>
2024-04-15 21:37:38 +0100

    View & download poster

--- settings.py
+++
@@ -1,7 +0,0 @@
-import os
-
-BASE_DIR = os.getcwd()
-DATA_DIR = os.path.join(BASE_DIR, 'data')
-IMAGES_DIR = os.path.join(BASE_DIR, 'images')
-
-poster_path = os.path.join(IMAGES_DIR, 'Partick_litter_Plan')
