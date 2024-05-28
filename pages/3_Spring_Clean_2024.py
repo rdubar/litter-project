7018323bbe04e295c0ac7d18ab10aee05bb22c02
Roger Dubar <rdubar@gmail.com>
2024-05-28 13:26:21 +0100

    Update events for June 2024

--- pages/3_Spring_Clean_2024.py
+++ pages/3_Spring_Clean_2024.py
@@ -2,10 +2,8 @@ import streamlit as st
 
 st.title("Spring Clean Scotland 2024 🌿") 
 
-st.image('images/Spring_Clean_Scotland_2024.jpg', width=400)
+st.image('images/PartickActionJune2024.jpg', width=500)
 
 """
-Sign up for the [Lidl Week of Action - Spring Clean](https://www.eventbrite.com/cc/lidl-week-of-action-spring-clean-2024-2410379), 12-24 April 2024.
-
-The Partick event is [Tuesday 23 April 2024, 10am-12pm. Meet at the Partick Lidl store](https://www.eventbrite.com/e/spring-clean-lidl-week-of-action-litter-pick-partick-tickets-845826318287?aff=odcleoeventsincollection&keep_tld=1).
+Sign up for the next [Partick Action For Litter](https://www.facebook.com/PartickActiononLitter) cleanup event on 30 June 2024.
 """
